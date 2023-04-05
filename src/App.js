7dfb8edee12f95cc3ee02014eda53eedca312fe9
zafaremon20 <zafar emon>
2023-04-05 22:47:34 +0600

    initialized project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Editor from "./components/Editor/editor";
+import JoditEditor from "./components/JoditEditor/JoditEditor";
+
+export default function App() {
+  return (
+    <div className="">
+      Editor
+      <Editor />
+      <JoditEditor />
+    </div>
+  );
+}
